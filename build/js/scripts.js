eee78b8725193f3b1cd0a3728ae75429679e3220
dima-kulikov <skaizy44@gmail.com>
2020-12-09 22:08:26 +0200

    dsd
    sdsd

---
+++ build/js/scripts.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var day = document.getElementById('day');
+var hour = document.getElementById('hour');
+var minutes = document.getElementById('minutes');
+var seconds = document.getElementById('seconds');
+var today, tsec, tmin, thour; // НАЧАЛО КОДА
+
+var now = new Date(); // IE и FF по разному отрабатывают getYear()
+
+var timeend = new Date(now.getYear() > 1900 ? now.getYear() + 1 : now.getYear() + 1901, 0, 1); // для задания обратного отсчета до определенной даты укажите дату в формате:
+// timeend= new Date(ГОД, МЕСЯЦ-1, ДЕНЬ);
+// Для задания даты с точностью до времени укажите дату в формате:
+// timeend= new Date(ГОД, МЕСЯЦ-1, ДЕНЬ, ЧАСЫ-1, МИНУТЫ);
+
+function time() {
+  today = new Date();
+  today = Math.floor((timeend - today) / 1000);
+  tsec = today % 60;
+  today = Math.floor(today / 60);
+  if (tsec < 10) tsec = '0' + tsec;
+  tmin = today % 60;
+  today = Math.floor(today / 60);
+  if (tmin < 10) tmin = '0' + tmin;
+  thour = today % 24;
+  today = Math.floor(today / 24); // Присваеваем код под нужный id
+
+  document.getElementById('day').innerHTML = today + " дней ";
+  document.getElementById('hour').innerHTML = thour + " часов";
+  document.getElementById('minutes').innerHTML = tmin + " минут";
+  document.getElementById('seconds').innerHTML = tsec + " секунд";
+  window.setTimeout("time()", 1000);
+}
+
+window.addEventListener('load', time);
